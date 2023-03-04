7f78a043affa3759217a580ae9de77a40fc57ef2
ali-mohammed0419 <ali.mohammed0419@gmail.com>
2023-03-04 16:38:06 -0600

    ~40:00

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom'
 
 const STYLES = ['btn--primary', 'btn--outline']
 
+const SIZES = ['btn--medium', 'btn--large']
+
 export const Button = ({
     children, 
     type, 
@@ -12,4 +14,17 @@ export const Button = ({
     buttonSize
 }) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button
+            className={'btn ${checkButtonStyle} ${checkButtonSize}'}
+            onClick={onClick}
+            type={type}>
+                {children}
+            </button>
+        </Link>
+    )
 }
